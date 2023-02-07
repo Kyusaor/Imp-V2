8bab1524de0f8a2fd91aa8d0be601a294642a035
Kyusakick <banque.cdl@gmail.com>
2023-02-07 20:16:18 +0100

    fix jquery

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { Client } from "discord.js";
-import Config from "../data/config.js";
+import { Config } from "../data/config.js";
 
 let bot = new Client(Config.clientParam);
-
 bot.login(Config.token);
 
 
+//Var initialization
+let kyu;
+
 //Executed at boot
-bot.on('ready', () => {
-    console.log('test')
+bot.on('ready', async () => {
+    kyu = await bot.users.fetch(Config.kyu);
+    console.log();
 })
