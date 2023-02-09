046bbb48ce72ea5fbf3d6c45c01812176805568f
Kyusakick <banque.cdl@gmail.com>
2023-02-09 16:53:05 +0100

    add modal constructor

--- src/core/models/newMemberForm.ts
+++ src/core/models/newMemberForm.ts
@@ -1,6 +1,7 @@
-import { GuildMember } from "discord.js";
+import { GuildMember, TextChannel } from "discord.js";
 import { bot } from "../../main.js";
 import { Constants } from "./constants.js";
+import { Methods } from "./methods.js";
 
 export class MemberIncome {
 
@@ -15,4 +16,10 @@ export class MemberIncome {
         this.guild = guild;
     }
 
+    async createModal(member: GuildMember) {
+        let validChannel = await bot.channels.fetch(Constants.channelsId.VALIDATION_CHANNEL) as TextChannel;
+        if(!validChannel) throw "No valid channel found (MemberIncomecreateModal)";
+
+        let payload = await Methods.constructModal(member);
+    }
 }
