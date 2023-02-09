647ba1ef1f8079dd61533a0e44f9809f0f907b23
Kyusakick <banque.cdl@gmail.com>
2023-02-09 15:51:25 +0100

    adding constructor

--- src/core/models/newMemberForm.ts
+++ src/core/models/newMemberForm.ts
@@ -1,11 +1,18 @@
-import { GuildMember} from "discord.js";
+import { GuildMember } from "discord.js";
+import { bot } from "../../main.js";
+import { Constants } from "./constants.js";
 
 export class MemberIncome {
 
     member: GuildMember;
+    nickname: string | undefined;
+    guild: "imp" | "invited"
 
-    constructor (member: GuildMember) {
+
+    constructor(member: GuildMember, nick: string | undefined, guild: "imp" | "invited") {
         this.member = member;
+        this.nickname = nick;
+        this.guild = guild;
     }
 
 }
