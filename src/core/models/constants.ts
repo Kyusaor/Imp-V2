332fa5d92c89fcd5d33c62bc95b0c2a57ee4f120
Kyusakick <banque.cdl@gmail.com>
2023-02-09 15:50:59 +0100

    adding constant file

---
+++ src/core/models/constants.ts
@@ -0,0 +1,9 @@
+export abstract class Constants {
+
+    static readonly kyu = "370293090407153665"
+
+    static readonly channelsId = {
+        VALIDATION_CHANNEL: "1073252249926631425",
+        R4_CHECKOUT: "802883413806940172"
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { Client } from "discord.js";
 import { Config } from "../data/config.js";
+import { Constants } from "./core/models/constants.js";
 
 import { BotText } from "./core/text/botText.js";
 import { Utils } from "./core/utils.js";
 
 let bot = new Client(Config.clientParam);
+export { bot };
 bot.login(Config.token);
 
 
@@ -13,6 +15,6 @@ let kyu;
 
 //Executed at boot
 bot.on('ready', async () => {
-    kyu = await bot.users.fetch(Config.kyu);
+    kyu = await bot.users.fetch(Constants.kyu);
     console.log(Utils.displayConsoleHour() + bot.user?.username + BotText.console.READY);
 })
