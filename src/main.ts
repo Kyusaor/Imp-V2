cd7fe0cac909d986e3eec8f84c7bd731ff14b4a8
Kyusakick <banque.cdl@gmail.com>
2023-02-15 15:21:51 +0100

    format

--- src/main.ts
+++ src/main.ts
@@ -8,24 +8,26 @@ import { Utils } from "./core/utils.js";
 
 
 //Var initialization
-let kyu:User, ImpServer:Guild;
+let kyu:User
+let ImpServer:Guild;
+
 let bot = new Client(Config.clientParam);
 bot.login(Config.token);
+
 
 //Executed at boot
 bot.on('ready', async () => {
     kyu = await bot.users.fetch(Constants.kyu);
     ImpServer = await bot.guilds.fetch(Constants.ImpServerId);
     console.log(Utils.displayConsoleHour() + bot.user?.username + BotText.console.READY);
-})
-
-export { bot, ImpServer };
-
-
+});
 
 //Script execution
 
 bot.on('interactionCreate', async intera => {
 
     if(intera.isChatInputCommand()) Methods.commandInteractionHandler(intera);
-})
+});
+
+
+export { bot, ImpServer };
