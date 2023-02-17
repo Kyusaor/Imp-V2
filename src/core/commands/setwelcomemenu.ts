6ded9cb5af601d4807abcf8d18af55fbd2e2b4c8
Kyusakick <banque.cdl@gmail.com>
2023-02-17 19:04:37 +0100

    finishing first version setwelcomemenu

--- src/core/commands/setwelcomemenu.ts
+++ src/core/commands/setwelcomemenu.ts
@@ -1,4 +1,4 @@
-import { ActionRowBuilder, SlashCommandBuilder, StringSelectMenuBuilder, TextBasedChannel } from "discord.js";
+import { ActionRowBuilder, CommandInteraction, EmbedBuilder, SlashCommandBuilder, StringSelectMenuBuilder, TextBasedChannel } from "discord.js";
 import { ImpServer, bot } from "../../main.js";
 import { Constants } from "../models/constants.js";
 
@@ -7,34 +7,49 @@ export const CommandBuilder = new SlashCommandBuilder()
     .setDescription(`Redéfinit le panneau d'accueil`)
     .setDefaultMemberPermissions(0)
 
-export async function run () {
+export async function run (intera: CommandInteraction) {
 
-    let r4list = await ImpServer.members.cache
-    .filter(m => m.roles.cache.map(m=> m.name).includes("R4"))
-    .map(m => m.nickname || m.user.username)
-
-    let welcomeMenuBuild = constructMenu(r4list);
-
+    intera.deferReply({ ephemeral: true });
+    let r4list = await listR4();
+    let welcomeMenuBuild = constructIncomeMenu(r4list);
+    let welcomeMenuEmbed = constructIncomeEmbed();
     let welcomeChannel = await bot.channels.fetch(Constants.channelsId.VALIDATION_CHANNEL) as TextBasedChannel;
-    welcomeChannel.send({components: [welcomeMenuBuild]});
-
+    await welcomeChannel.send({embeds: [welcomeMenuEmbed], components: [welcomeMenuBuild]});
+    intera.editReply({ content: "Le menu a bien été envoyé"})
 }
 
 
-function constructMenu (r4list: string[]) {
+function constructIncomeEmbed () {
+    let embed = new EmbedBuilder()
+        .setTitle("**Qui vous a invité?**")
+        .setDescription('Cliquez en dessous pour choisir le pseudo du r4 qui vous a invité sur le discord')
+
+    return embed;
+}
+
+function constructIncomeMenu (r4list: string[]) {
 
     let menu = new StringSelectMenuBuilder()
         .setCustomId(`r4-select`)
-        .setPlaceholder("*Aucune option sélectionnée*")
+        .setPlaceholder("Cliquez ici pour choisir")
 
-    for(let r4 in r4list){
+    for(let r4 of r4list){
         menu.addOptions({
             label: r4,
-            description: "",
+            description: " ",
             value: r4,
         })
     }
 
     let row = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(menu);
     return row;
+}
+
+async function listR4() {
+    await ImpServer.members.list({ limit: 300 });
+    let r4list = await ImpServer.members.cache
+        .filter(m => m.roles.cache.has(Constants.rolesId.R4))
+        .map(m => m.nickname || m.user.username);
+    r4list.push("Quelqu'un d'autre");
+    return r4list;
 }
