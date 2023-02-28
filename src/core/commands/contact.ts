9b52c789cba23be0fc8cc98d0da6149d0e2a6ce1
Kyusakick <banque.cdl@gmail.com>
2023-02-28 18:14:25 +0100

    add embed creation (1 contact found)

--- src/core/commands/contact.ts
+++ src/core/commands/contact.ts
@@ -7,6 +7,7 @@ import {
     User 
 } from "discord.js";
 import { readFileSync } from "fs";
+import { Config } from "../../../data/config.js";
 import { Constants } from "../models/constants.js";
 import { Utils } from "../utils.js";
 import { contactSheet } from "./annuaire.js";
@@ -33,7 +34,10 @@ export async function run(intera:ChatInputCommandInteraction) {
         return Utils.interaReply(Constants.text.contacts.memberNotFound, intera);
 
     let profiles = findContactElement(pseudo, discord, db);
-    if(!Object.keys(profiles)) return Utils.interaReply(profiles as string, intera);
+    if(typeof profiles == 'string') return Utils.interaReply(profiles as string, intera);
+
+    let payload = buildContactEmbed(profiles);
+    Utils.interaReply(payload, intera);
 }
 
 export async function autocompleteManager(intera:AutocompleteInteraction, db:contactSheet[]) {
@@ -51,21 +55,64 @@ function findContactElement(pseudo:string | null, discord:User | null, db:contac
     if(!pseudo && discord) {
         let id = discord.id;
         output = db.filter(e => e.user == id);
-        if(!output) output = "Aucune fiche contact correspondante";
+        if(output.length == 0) output = "Aucune fiche contact ne correspond à ce membre";
     }
     else if(pseudo && !discord) {
         output = db.filter(e => e.pseudo == pseudo);
-        if(!output) output = "Aucune fiche contact correspondante";
+        if(output.length == 0) output = "Aucune fiche contact correspondante à ce pseudo";
     }
     else {
         output = db.filter(e => (e.pseudo == pseudo) && (e.user == discord?.id));
-        if(!output) output = "Cet utilisateur discord ne possède pas de compte enregistré avec ce pseudo sur l'annuaire";
+        if(output.length == 0) output = "Cet utilisateur discord ne possède pas de compte enregistré avec ce pseudo sur l'annuaire";
     }
 
     return output;
 }
 
-function buildContactEmbed() {
-    
+function buildContactEmbed(data:contactSheet[]) {
+    let payload:InteractionReplyOptions = {};
+
     let embed = new EmbedBuilder()
+        .setTitle(`Fiche contact`)
+        .setThumbnail(`https://discord.com/assets/5f8aee4f266854e41de9778beaf7abca.svg`)
+        .setDescription(`En cas de souci, mentionnez <@${Constants.kyu}>`)
+        .setFooter({text: `Pour copier le numéro, faites un appui long dessus`})
+
+    switch(data.length) {
+        case 0:
+            payload.content = Constants.text.errors.errorFetchMember;
+            break;
+
+        case 1:
+            let profile = data[0];
+            embed.addFields(
+                {
+                    name: `Pseudo en jeu`,
+                    value: `${profile.pseudo}`
+                },
+                {
+                    name: `Compte discord`,
+                    value: `<@${profile.user}>`
+                },
+                {
+                    name: `Numéro de téléphone`,
+                    value: `${profile.displayPhoneNumber()}`
+                },
+                {
+                    name: `Renfo`,
+                    value: `${profile.renfo}`
+                },
+                {
+                    name: `Membres à contacter`,
+                    value: `${profile.displayMates()}`
+                },
+            )
+            payload = { embeds: [embed] }
+            break;
+        
+        default:
+            break;
+    }
+
+    return payload;
 }
