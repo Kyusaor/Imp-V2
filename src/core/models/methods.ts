b4f0d2314780a6a924818fb1f28bdce0fcf9e7e1
Kyusakick <banque.cdl@gmail.com>
2023-03-02 18:35:01 +0100

    Remove useless forgotten import

--- src/core/models/methods.ts
+++ src/core/models/methods.ts
@@ -30,7 +30,6 @@ export class Methods {
     //Autocomplete handler
     static async autocompleteHandler(intera:AutocompleteInteraction) {
         let db = JSON.parse(readFileSync('./data/contacts.json', 'utf-8'));
-        let command = await import(`../utils.js`);
         await Utils.autocompleteManager(intera, db);
     }
 
